82d8a516e8f1f295e9cebd88144fe36c886d2986
Tilman Kerl <tilman.kerl@uni-konstanz.de>
2020-06-24 15:38:58 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Basic Python Flask Url Shorten Service
+
+This little project was a challenge to be completed in under one hour. Hence there maybe a few flaws here and there. See the Security and Validation Notice.
+
+## Service
+For every long URL (and the `time.time()` value) a SHA-256 hash is generated.
+A random part of the hash is then used to represent the long URL.
+
+Let's look at an example, we have a long url: `https://github.com/MisterXY89`. The app gives us
+`short.io/b61982` (Notice that `short.io` is an example `BASE_URL`). The app then looks up the
+hash code `b61982` in the DB and returns the long URL `https://github.com/MisterXY89`
+
+## Security & Validation Notice
+At the moment the URL input gets not validated and no escaped! Keep that in mind when using.
+
+Furthermore no extra precautions for security have been taken.
+
+
+## UI
+The user interface is very basic since this was created with a time limit. I used the CSS framework [Beauter](beauter.outboxcraft.com/).
+
+## Database
+This implementation uses a sqlite3 database, if you want to create the table yourself, here is the SQL statement:
+
+```sql
+CREATE TABLE "urls" (
+	"id"	INTEGER PRIMARY KEY AUTOINCREMENT,
+	"shortUrl"	TEXT NOT NULL DEFAULT '<<CUSTOM DEFAULT>>',
+	"longUrl"	TEXT NOT NULL,
+	"hash"	TEXT NOT NULL UNIQUE,
+	"created"	TEXT NOT NULL
+)
+```
+## Run it on your machine
+In `app.py` you have to configure some options:
+```python
+# YOUR TIMEZONE
+os.environ["TZ"] = "Europe/Berlin"
+time.tzset()
+
+# HOSTNAME, here let's say it is short.io / localhost:500 for dev
+BASE_URL = "short.io"
+
+# URL_DB FILENAME
+DB_FILE = "urls.db"
+```
+Make sure you created the DB, close the connection from the editor and install the requirements.
+
+Then you can simply run the application via: `flask run` or `python3 app.py`.
---
+++ app.py
@@ -0,0 +1,119 @@
+
+import os
+import time
+import random
+import sqlite3
+from hashlib import sha256
+from flask import Flask, redirect, url_for, render_template, request
+
+app = Flask("urlShortener")
+
+# disable caching
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
+
+# YOUR TIMEZONE
+os.environ["TZ"] = "Europe/Berlin"
+time.tzset()
+
+# HOSTNAME, here let's say it is short.io / localhost:500 for dev
+BASE_URL = "short.io"
+
+# URL_DB FILENAME
+DB_FILE = "urls.db"
+
+
+def randomInterval():
+    start = random.randint(0, 150)
+    end = start + 7
+    return {"start": start, "end": end}
+
+
+def create_connection():
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(DB_FILE)
+    except Exception as e:
+        print(e)
+
+    return conn
+
+
+def updateUrlDb(urlDict):
+    conn = create_connection()
+    sql = f"INSERT INTO urls(shortUrl,longUrl,hash, created) VALUES('{urlDict['shortUrl']}','{urlDict['longUrl']}','{urlDict['hash']}','{urlDict['time']}')"
+    cur = conn.cursor()
+    cur.execute(sql)
+    conn.commit()
+    return cur.lastrowid
+
+
+def readLongUrl(hash):
+    conn = create_connection()
+    sql = f"SELECT longUrl from urls WHERE hash LIKE '{hash}'"
+    cur = conn.cursor()
+    cur.execute(sql)
+    res = cur.fetchall()
+    conn.commit()
+    try:
+        longUrl = res[0]
+        longUrl = str(longUrl[0])
+    except Exception as e:
+        longUrl = "localhost:5000"
+    finally:
+        return longUrl
+
+
+@app.route('/<string:hash>/')
+def redirectToUrl(hash):
+    longUrl = readLongUrl(hash)
+    return redirect(longUrl, code=302)
+
+
+@app.route("/a/result")
+def result():
+    if "longUrl" in request.args and "shortUrl" in request.args:
+        longUrl = request.args["longUrl"]
+        shortUrl = request.args["shortUrl"]
+        return render_template("result.html", shortUrl = shortUrl, longUrl = longUrl)
+    return redirect(url_for("index"))
+
+@app.route("/a/shorten", methods=["POST"])
+def shorten():
+    url = request.form["longUrl"]
+    urlHash = sha256((url+str(time.time())).encode('utf-8')).hexdigest()
+    interval = randomInterval()
+    shortUrlHash = urlHash[interval["start"]:interval["end"]]
+
+    print(url)
+    print(interval)
+    print(urlHash)
+
+    # TODO: escape url!!
+    shortUrl = BASE_URL + "/" + shortUrlHash
+
+    urlDict = {
+        "shortUrl": shortUrl,
+        "longUrl": url,
+        "time": str(time.time()),
+        "hash": shortUrlHash
+    }
+    res = updateUrlDb(urlDict)
+    print(res)
+    return redirect(url_for("result", shortUrl = shortUrl, longUrl = url))
+    return urlDict
+
+
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run()
